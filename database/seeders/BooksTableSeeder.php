e7720844501f4fe729b5bc8610b01f550fbee744
Johannes Schoenboeck <johannes.schoenboeck@fh-hagenberg.at>
2021-03-05 11:14:23 +0100

    Added Book Migration and Seeder

---
+++ database/seeders/BooksTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use DateTime;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class BooksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('books')->insert([
+            'title' => Str::random(100),
+            'isbn' => "1234567890",
+            'subtitle' => Str::random(100),
+            'rating' => 10,
+            'description' => Str::random(1000),
+            'published' => new DateTime(),
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+    }
+}
