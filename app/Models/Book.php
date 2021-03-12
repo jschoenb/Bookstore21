e9e165ade9e15ec4213d262574b06db7e5bb9e4f
Johannes Schönböck <johannes.schoenboeck@fh-hagenberg.at>
2021-03-12 10:26:16 +0100

    Added Image Model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Book extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable
+     * @var array
+     */
+    protected  $fillable = ['isbn','title','subtitle','published','rating',
+        'description','user_id'];
+
+
+    public function isFavourite() : bool {
+        return $this->rating >= 8;
+    }
+
+    public function images() : HasMany {
+        return $this->hasMany(Image::class);
+    }
 }
--- database/seeders/BooksTableSeeder.php
+++ database/seeders/BooksTableSeeder.php
@@ -16,16 +16,25 @@ class BooksTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('books')->insert([
-            'title' => Str::random(100),
-            'isbn' => "1234567890",
-            'subtitle' => Str::random(100),
-            'rating' => 10,
-            'description' => Str::random(1000),
-            'published' => new DateTime(),
-            'created_at' => date("Y-m-d H:i:s"),
-            'updated_at' => date("Y-m-d H:i:s")
-        ]);
+        $book = new \App\Models\Book;
+        $book->title = "Herr der Ringe";
+        $book->isbn = "1234567890";
+        $book->subtitle = "Die Rückkehr des Königs";
+        $book->rating = 10;
+        $book->description = "Letzter Teil der Trilogie";
+        $book->published = new DateTime();
+        $book->save();
+
+        // add images to book
+        $image1 = new \App\Models\Image;
+        $image1->title = "Cover 1";
+        $image1->url = "https://images-na.ssl-images-amazon.com/images/I/61h%2BnIJyVFL._SX333_BO1,204,203,200_.jpg";
+
+        $image2 = new \App\Models\Image;
+        $image2->title = "Cover 2";
+        $image2->url = "https://images-eu.ssl-images-amazon.com/images/I/516KV5tjulL._AC_US327_FMwebp_QL65_.jpg";
+        $book->images()->saveMany([$image1,$image2]);
+        $book->save();
 
     }
 }
